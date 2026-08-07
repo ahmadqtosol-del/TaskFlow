8635980947a35450870aadd3d9dfa9304549d0f4
Ahmad Qtos <ahmadqtosol@gmail.com>
2026-08-08 00:17:41 +0500

    tunneling

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -18,7 +18,7 @@ async function api(path, options = {}) {
     headers.set("Content-Type", "application/json");
   }
 
-const API_URL = "http://se-mlm-01.velrix.net:4050/api";
+const API_URL = "https://rogers-telecom-bacteria-whose.trycloudflare.com/api";
 
 const res = await fetch(`${API_URL}${path}`, {
   ...options,
